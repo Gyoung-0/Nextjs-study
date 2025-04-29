33d4654e8ec75952500c6e74f2c80391b2c43213
Gyoung-0 <hkhj77777@gmail.com>
2025-04-29 11:56:40 +0900

    feat: Dynamic Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
+import "../styles/global.css";
 import { Metadata } from "next";
 import Navigation from "../components/navigation";
 
-export const metadata :Metadata = {
-  title:{
+export const metadata: Metadata = {
+  title: {
     template: "%s |T Next Movies",
-    default: "Loading..."
+    default: "Loading...",
   },
   description: "The best movies on the best framework",
 };
