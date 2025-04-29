262b00cbedf874f24f625d061f87e86371e886f2
Gyoung-0 <hkhj77777@gmail.com>
2025-04-29 09:32:49 +0900

    Data Fetching error handling

--- app/(moives)/movies/[id]/page.tsx
+++ app/(moives)/movies/[id]/page.tsx
@@ -1,5 +1,7 @@
+import { Suspense } from "react";
 import { API_URL } from "../../../(home)/page";
-
+import MovideInfo from "../../../../components/movie-info";
+import MovideVideos from "../../../../components/movie-videos";
 
 export default async function MovieDetail({
   params,
@@ -7,6 +9,15 @@ export default async function MovieDetail({
   params: Promise<{ id: string }>;
 }) {
   const { id } = await params;
-  
-  return <div></div>
+
+  return (
+    <div>
+      <Suspense fallback={<h1>Loading movie info</h1>}>
+        <MovideInfo id={id} />
+      </Suspense>
+      <Suspense fallback={<h1>Loading movie videos</h1>}>
+        <MovideVideos id={id} />
+      </Suspense>
+    </div> 
+  );
 }
